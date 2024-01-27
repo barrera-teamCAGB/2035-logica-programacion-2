fc99e6c4f4409c235945ff65a1f742db5856db11
barrera-teamCAGB <156938736+barrera-teamCAGB@users.noreply.github.com>
2024-01-27 14:03:27 -0600

    Actividad 4: Genera un numero aleatorio

--- app.js
+++ app.js
@@ -9,6 +9,8 @@
 // Actividad 3: Trabajando con DOM
 // un ejercio deonde cambiemos el texto del h2 del archivo html cada que se preciona un boton, la respuesta es la opcion C, recordar que el document.querySelector nos indica que elemnto del DOM queremos utilizar, para estos es necesario conoce la mayoria de las etiquetas de html como los hedear, parrafos, botones, listas, etc
 
+// Actividad 4: Genera un numero aleatorio
+// todas las funciones por buena practica deben contener al final el return; regresen o no un valores, podemos utilizar variables de bloque que solo funcionaran dentro de la funcion y podemos devolver con return esa o esas variables por ejemplo return variable1; o variables globales de largo alcance, hay que tener cuidado con ellas para no repetir o crear confuncion, en la funcion asignarTextoElemento no regresa ningun valor y en la funcion generarNumeroSecreto regresara un valor
 
 let numeroSecreto = generarNumeroSecreto();
 let intentos = 0;
