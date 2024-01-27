e61bad5e84592381663ede504a1f5ea279dde1b3
barrera-teamCAGB <156938736+barrera-teamCAGB@users.noreply.github.com>
2024-01-27 13:50:01 -0600

    Actividad 3: Trabajando con DOM

--- app.js
+++ app.js
@@ -6,6 +6,9 @@
 // Actividad 2: Asigna texto a un elemento html
 // Cuando vemos que el código se repite mas de una vez podemos simplificar ese código con una función y usar parámetros asi simplificaremos este y podremos volver a utilizarlo mas adelante, como el caso de la funcion asignarTextoElemento(), cada que queramos agregar un text a un elemnto bastra con llamar a esa funcion y colocarle los 2 parametros que nos pide
 
+// Actividad 3: Trabajando con DOM
+// un ejercio deonde cambiemos el texto del h2 del archivo html cada que se preciona un boton, la respuesta es la opcion C, recordar que el document.querySelector nos indica que elemnto del DOM queremos utilizar, para estos es necesario conoce la mayoria de las etiquetas de html como los hedear, parrafos, botones, listas, etc
+
 
 let numeroSecreto = generarNumeroSecreto();
 let intentos = 0;
