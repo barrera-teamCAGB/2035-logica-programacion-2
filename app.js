6249eaa25501d64cb43eb5a023d9f0e9461eeca2
barrera-teamCAGB <156938736+barrera-teamCAGB@users.noreply.github.com>
2024-01-26 19:48:51 -0600

    Actividad 5: Evento a un click y primera funcion

--- app.js
+++ app.js
@@ -19,3 +19,17 @@ parrafo.innerHTML = "Indica un numero del 1 al 10"
 // al método document.querySelector() debe recibir una cadena como argumento y debe estar entre comillas para que este correcto
 // innerHTML debe tener un valor asignado debe estar entre comillas (simples o dobles) para que se trate como una cadena de texto. Sin las comillas, JavaScript intentará interpretar el texto como una variable, lo que provocará un error si la variable no está definida.
 ///////////// fin de la actividad 4 /////////////
+
+
+///////////// Actividad 5: Evento a un click y primera funcion /////////////
+// cualquier interacion desde la interfaz web o frontend, hace una acion como la de desplazar el mouse, colocar el puntero en algun lugar, seleccionar, un click, 2 click, etc, tambien llamdo evento https://developer.mozilla.org/es/docs/Learn/JavaScript/Building_blocks/Events ver tambien https://developer.mozilla.org/es/docs/Web/Events
+
+// la creacion de funciones sigue el mismo concepto de variable, nada de caracteres especiales, que no inicie con numer, no tenga espacios, y usar camelCase, estas funciones se colocan en el archivo html donde estara el evento y termina entre parentesis y dentro del parentesis puedn ir calos o parametros y esta funcion a su vez puede o no regresar un valos
+// miFuncion(); esto va en el evento por ejemplo onclick="miFuncion();" este evento le pertenece ala etiqueta button, puede ser cualquier otra mas que lo acepte como la de una caja de texto la pagina completa etc
+// ejemplo en nuestro codigo
+// en el html en el primer boton colocaremos el evento onclick y dentro nuestra funcio donde aqui la crearemos
+// existen varias formas de llamar a la funcion aqui la tradicional donde se declara
+function intentoUsuario() {
+    alert ("click desde el boton");
+}
+///////////// fin de la actividad 5 /////////////
